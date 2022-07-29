56ae10b6723b674b20b89d3307bf22382d7b2730
Roman <chess.terr.dev@yandex.ru>
2022-07-29 16:05:27 +0300

    Changed the implementation of the config

--- settings/config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-
-return [
-    'jsonDataPath' => __DIR__ . '/../../data/lpu.json'
-];
---
+++ src/Config.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace BARSGroupTestTask;
+
+final class Config
+{
+    public const JSON_DATA_PATH = __DIR__ . '/../data/lpu.json';
+    public const AUTH_SALT = 'BARSGroupTestTask';
+}
